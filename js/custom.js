1148aaed5da4546aff8533c86caeedc15196979b
Dana Weiss <mynameisdanaweiss@gmail.com>
2019-03-01 21:16:52 -0800

    reload page setInterval in js/custom.js

--- js/custom.js
+++ js/custom.js
@@ -5,11 +5,17 @@ function getRandomNum() {
 }
 
 // target html body background color and populate with randomly generated rgb values
+// found the neat rgb join here: https://stackoverflow.com/questions/14323082/why-doesnt-backgroundcolor-rgba-b-c-work
 function changeBackground() {
   let a = getRandomNum();
   let b = getRandomNum();
   let c = getRandomNum();
   document.body.style.backgroundColor = 'rgb(' + [a,b,c].join(',') + ')';
 }
+
+//reloads the current document after 6 seconds (6000ms)
+window.setInterval(function () {
+  location.reload();
+},6000);
 
 document.addEventListener("load", changeBackground())
