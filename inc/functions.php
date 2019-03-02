19383b51455999e46412be05c48446e527d53b72
Dana Weiss <mynameisdanaweiss@gmail.com>
2019-03-01 18:30:49 -0800

    create multidimensional quote array in inc/functions.php

--- inc/functions.php
+++ inc/functions.php
@@ -1,7 +1,38 @@
-// PHP - Random Quote Generator
-
+<?php
 // Create the Multidimensional array of quote elements and name it quotes
 // Each inner array element should be an associative array
+$quotes = [
+  [
+    "quote" => "Only to the extent that we expose ourselves over and over to annihilation can that which is indestructible in us be found.",
+    "source" => "Pema Chodron",
+    "citation" => "When Things Fall Apart",
+    "year" => "2016",
+  ],
+  [
+    "quote" => "She wasn't doing a thing that I could see, except standing there leaning on the balcony railing, holding the universe together.",
+    "source" => "J.D. Salinger",
+    "citation" => "A Girl I Knew",
+    "year" => "1948",
+  ],
+  [
+    "quote" => "Have patience with all things, but chiefly have patience with yourself. Do not lose courage in considering your own imperfections but instantly set about remedying them - every day begin the task anew.",
+    "source" => "Saint Francis de Sales",
+  ],
+  [
+    "quote" => "The way things are does not determine the way they ought to be.",
+    "source" => "Michael J. Sandel",
+    "citation" => "Justice: What's the Right Thing to Do?",
+    "year" => "2009",
+  ],
+  [
+    "quote" => "Courage is being scared to death, but saddling up anyway.",
+    "source" => "John Wayne",
+  ],
+];
+
+//testing 
+echo $quotes[0]['quote'];
+
 
 
 
@@ -10,3 +41,5 @@
 
 
 // Create the printQuote funtion and name it printQuote
+
+?>
